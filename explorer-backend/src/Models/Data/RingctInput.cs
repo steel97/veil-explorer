9c3b5bdab4ea7640aa5fe2b40d2e7609838d5449
Ivan Yv <steel-97@mail.ru>
2021-12-25 00:18:13 +0300

    Removed unused models

--- explorer-backend/src/Models/Data/RingctInput.cs
+++
@@ -1,9 +0,0 @@
-namespace explorer_backend.Models.Data;
-
-public class RingctInput
-{
-    public Guid id { get; set; }
-    public Guid tx_input_id { get; set; }
-    public string? txid_hex { get; set; }
-    public long vout_n { get; set; }
-}
