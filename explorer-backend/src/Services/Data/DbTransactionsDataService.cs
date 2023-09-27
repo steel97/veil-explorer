6ca38245a9f15857852c842685ed0fa60a2d9e90
waku <wkau@waifu.club>
2023-09-27 19:04:16 +0300

    added implementation

--- explorer-backend/src/Services/Data/DbTransactionsDataService.cs
+++ explorer-backend/src/Services/Data/DbTransactionsDataService.cs
@@ -1,26 +1,21 @@
 using ExplorerBackend.Models.Data;
+using ExplorerBackend.Persistence.Repositories;
 
 namespace ExplorerBackend.Services.Data;
 
 public class DbTransactionsDataService : ITransactionsDataService
 {
-    public Task<Transaction?> GetTransactionByIdAsync(string txid, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    private readonly ITransactionsRepository _transactionsRepository;
+    public DbTransactionsDataService(ITransactionsRepository transactionsRepository) => _transactionsRepository = transactionsRepository;
+    public Task<Transaction?> GetTransactionByIdAsync(string txid, CancellationToken cancellationToken = default) =>
+        _transactionsRepository.GetTransactionByIdAsync(txid, cancellationToken);
 
-    public Task<TransactionExtended?> GetTransactionFullByIdAsync(string txid, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<TransactionExtended?> GetTransactionFullByIdAsync(string txid, CancellationToken cancellationToken = default) =>
+        _transactionsRepository.GetTransactionFullByIdAsync(txid, cancellationToken);
 
-    public Task<List<TransactionExtended>?> GetTransactionsForBlockAsync(int blockHeight, int offset, int count, bool fetchAll, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<List<TransactionExtended>?> GetTransactionsForBlockAsync(int blockHeight, int offset, int count, bool fetchAll, CancellationToken cancellationToken = default) =>
+        _transactionsRepository.GetTransactionsForBlockAsync(blockHeight, offset, count, fetchAll, cancellationToken);
 
-    public Task<string?> ProbeTransactionByHashAsync(string txid, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<string?> ProbeTransactionByHashAsync(string txid, CancellationToken cancellationToken = default) => 
+        _transactionsRepository.ProbeTransactionByHashAsync(txid, cancellationToken);
 }
