21f118c54b2996c26b412a1144eb120ffb5e0b16
waku <pavel.django2@mail.ru>
2023-12-06 20:47:43 +0300

    redis config info addition

--- explorer-backend/src/Program.cs
+++ explorer-backend/src/Program.cs
@@ -35,7 +35,8 @@ bool rpcMode = builder.Configuration.GetSection("Explorer:RPCMode").Get<bool>();
 
 // Add services to the container.
 if(rpcMode)
-    // Redis-cli config: save="", activedefrag=yes, maxmemory=600mb, loglevel=warning, crash-log-enabled=no, crash-memcheck-enabled=no, protected-mode=yes
+    // Redis-cli config: save="", activedefrag=yes, maxmemory=524288000 (value should be in bytes),maxmemory-policy=volatile-lru, 
+    // loglevel=warning, crash-log-enabled=no, crash-memcheck-enabled=no, protected-mode=yes
     builder.Services.AddSingleton<IConnectionMultiplexer>(options =>    
         ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis") ?? ""));
 else
