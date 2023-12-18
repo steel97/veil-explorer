7c73643d45debfa0e224a3d5c7df6c9b5ad40f1c
waku <pavel.django2@mail.ru>
2023-12-18 15:52:37 +0300

    minor changes

--- explorer-backend/src/Configs/MemoryCacheConfig.cs
+++ explorer-backend/src/Configs/MemoryCacheConfig.cs
@@ -3,7 +3,7 @@ namespace ExplorerBackend.Configs;
 public class MemoryCacheConfig
 {
     public int Port { get; set; }
-    public string Host { get; set; }
+    public string? Host { get; set; }
     public int RedisMaxMemoryUsage { get; set; }
     public int OldestSimplifiedBlocksCacheCount { get; set; }
     public int SimplifiedBlocksCacheCount { get; set; }
