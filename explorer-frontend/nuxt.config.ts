6721219c5dbd756917ec58edc5c0494ac9d688b6
Ivan Yv <steel-97@mail.ru>
2024-07-12 00:27:21 +0300

    Updated packages

--- explorer-frontend/nuxt.config.ts
+++ explorer-frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    compatibilityDate: "2024-07-12",
     runtimeConfig: {
         public: {
             i18n: {
